81a029dc79bd05595d6e43e74608149d6d37d2ed
ethanrjs <ethanmrettinger@gmail.com>
2023-05-13 02:25:50 -0400

    FUCK ESMODULES!!! CJS FTW

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 5500;
 const REFRESH_INTERVAL = process.env.REFRESH_INTERVAL || 30000;
 
 // Serve public files
-app.use(serveStatic(path.join(import.meta.dir, "public")));
+app.use(serveStatic(path.join(__dirname, "public")));
 
 // Function to search and update package list
 async function refreshPackageList() {
     try {
-        const packagesPath = path.join(import.meta.dir, "packages");
+        const packagesPath = path.join(__dirname, "packages");
 
         const folders = await fs.readdir(packagesPath);
 
--- routes/packages.js
+++ routes/packages.js
@@ -19,7 +19,7 @@ packagesRouter.get("/", async (req, res) => {
     }
 
     const results = [];
-    const packagesPath = path.resolve(import.meta.dir, "../packages");
+    const packagesPath = path.resolve(i__dirname, "../packages");
 
     await Promise.all(packageNames.map(async (packageName, i) => {
         const packageResults = await Promise.all(filePaths[i].map(async (filePath) => {
